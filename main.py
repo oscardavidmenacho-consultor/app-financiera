517f983c4ab4ad3334785f894ed2e3d1e4f84f79
oscardavidmenacho-consultor <oscar.david.menacho@gmail.com>
2026-01-21 10:43:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -656,4 +656,5 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Error técnico: {e}")
 else:
-    st.info("👋 ¡Hola! Para usar esta App, primero descarga la plantilla del menú izquierdo, complétala y súbela. 🚀 Luego, ¡Descarga tu Reporte en Excel!")
+    st.info("👋 ¡Hola! Para usar esta App, primero descarga la plantilla del menú izquierdo, complétala y súbela.
+    🚀 Después, ¡Descarga tu Reporte en Excel!")
